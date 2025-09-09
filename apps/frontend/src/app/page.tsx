99587e839e87d96fb0a6ed8762bcb435a4aceaed
hnascx <henrique.nascimento12@hotmail.com>
2025-09-09 14:22:12 -0300

    chore: Install shadcn/ui

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "../components/ui/button"
 import "./styles/globals.css"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-black">
+    <div className="flex flex-col items-center justify-center h-screen bg-black space-y-4">
       <h1 className="text-white text-4xl font-bold">Boilerplate</h1>
+      <Button>Clique aqui</Button>
     </div>
   )
 }
