302e95b70889f5411a12dfee14a58d89aa59fbb9
hnascx <henrique.nascimento12@hotmail.com>
2025-09-09 14:18:31 -0300

    chore: Install tailwindcss

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import "./styles/globals.css"
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-black">
+      <h1 className="text-white text-4xl font-bold">Boilerplate</h1>
+    </div>
+  )
+}
